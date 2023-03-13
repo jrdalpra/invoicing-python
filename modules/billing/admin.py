b0325cb8808260acc678195b30edc1cbd548fb2b
José Volmei Dal Prá Junior <jose.dalprajunior@loadsmart.com>
2023-03-13 06:26:41 -0300

    Little improvements
    - added jazzmin to admin
    - fixed warnings when building
    - added inlinte items to invoice admin page

--- modules/billing/admin.py
+++ modules/billing/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 
 # Register your models here.
-from modules.billing.models.definitions import Invoice
+from modules.billing.models.definitions import Invoice, InvoiceItem
 
-admin.site.register(Invoice)
+
+class InvoiceItemForm(admin.TabularInline):
+    model = InvoiceItem
+    fk_name = "invoice"
+
+
+class InvoiceAdmin(admin.ModelAdmin):
+    inlines = [
+        InvoiceItemForm
+    ]
+
+    list_display = [
+        "id",
+        "seller_name",
+        "buyer_name",
+        "total"
+    ]
+
+    def seller_name(self, obj: Invoice):
+        return str(obj.seller.name)
+
+    def buyer_name(self, obj: Invoice):
+        return str(obj.buyer.name)
+
+    def total(self, obj: Invoice):
+        return str(obj.total)
+
+
+admin.site.register(Invoice, InvoiceAdmin)
